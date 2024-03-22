d5c5697294dfa495eac413871e90fe4e0e429c4f
alex <alexmaxu12@gmail.com>
2024-03-22 22:16:11 +0100

    Initial Commit

---
+++ PokedexAlex/PokedexAlexApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexAlexApp.swift
+//  PokedexAlex
+//
+//  Created by Alex  on 22/3/24.
+//
+
+import SwiftUI
+
+@main
+struct PokedexAlexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
